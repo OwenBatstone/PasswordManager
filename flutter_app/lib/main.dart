13cd1d6c61b5fccb3fb352570fe6453b9e523f43
OwenBatstone <ogbatsto@lakeheadu.ca>
2026-03-06 20:21:47 -0500

    Added Login  Page

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -7,11 +7,6 @@ void main() async{
    
 }
 
-
-
-
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
--- flutter_app/lib/pages/login_page.dart
+++ flutter_app/lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class LoginPage extends StatefulWidget{ 
   const LoginPage({super.key});
 
@@ -11,6 +12,8 @@ class LoginPage extends StatefulWidget{
 
 class _LoginPageState extends State<LoginPage> { 
 
+
+final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 final TextEditingController emailController = TextEditingController(); 
 final TextEditingController passwordController = TextEditingController();
 
@@ -21,6 +24,60 @@ Widget build(BuildContext context) {
       backgroundColor: Colors.blue,
       title: const Text('Login')
     ),
+    body : Padding(
+      padding: EdgeInsets.all(10),
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "Email"
+              ),
+              //Validator to check for something
+              validator: (value) {
+                if(value == null || value.isEmpty)
+                {
+                  return "Enter Email";
+                }
+                return null;  
+              },
+              
+            ),
+            const SizedBox( 
+              height : 20
+              ),
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Password"
+                ),
+                validator: (value) { 
+                  if( value == null || value.isEmpty){ 
+                    return "Enter Password";
+                  }
+                  return null;
+                }
+                ),
+                const SizedBox(height: 20), 
+                ElevatedButton(onPressed: () {
+                  if( formKey.currentState!.validate()){ 
+                    //TODO CREATE new class with login logic
+                    // Steps planned to implement
+                    // hash then encrypt then compare to database 
+                    // could aso hash email if we really want?
+                    print('Email:${emailController.text}');
+                  }
+                }, 
+                child: const Text('Login'),
+                )
+          ],
+        ),
+      ),
+      )
   );
 
 }
